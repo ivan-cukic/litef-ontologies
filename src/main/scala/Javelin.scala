d318fbc76dac412553425fa237db56b930821e4f
Ivan Čukić <ivan.cukic@kde.org>
2014-05-04 15:14:52 +0200

    Fixes for the ontology

--- src/main/scala/Javelin.scala
+++ src/main/scala/Javelin.scala
@@ -12,43 +12,43 @@ import javelin.ontology.Implicits._
   * Ontology for Javelin internal concepts and properties
   */
 object Javelin {
-    lazy val NS = "https://raw.githubusercontent.com/ivan-cukic/litef-ontologies/master/misanu.ivan.javelin.ttl#"
+    lazy val NS = "http://ivan.fomentgroup.org/ontologies/javelin#"
+
+    /**
+     * Ontology namespace abbreviation (property)
+     * Abbreviation for the ontology's namespace
+     */
+    lazy val hasNamespaceAbbreviation = NS #> "hasNamespaceAbbreviation"
+
+    /**
+     * User visible (property)
+     * Should a class, resource or property be visible to the user
+     */
+    lazy val isUserVisible = NS #> "isUserVisible"
+
+    /**
+     * Modification time (property)
+     * The modification time of the subject (usually a file)
+     */
+    lazy val hasModificationTime = NS #> "hasModificationTime"
+
+    /**
+     * Ontology (class)
+     * Metadata about the ontology
+     */
+    lazy val Ontology = NS ## "Ontology"
+
+    /**
+     * Ontology namespace (property)
+     * Namespace for the ontology
+     */
+    lazy val hasNamespace = NS #> "hasNamespace"
 
     /**
      * Javelin Ontology^^http://www.w3.org/2001/XMLSchema#string (class)
      * Ontology for Javelin internal concepts and properties^^http://www.w3.org/2001/XMLSchema#string
      */
     lazy val metadata = NS ## "metadata"
-
-    /**
-     * Ontology namespace (property)
-     * Namespace for the ontology
-     */
-    lazy val sNamespace = NS #> "sNamespace"
-
-    /**
-     * User visible (property)
-     * Should a class, resource or property be visible to the user
-     */
-    lazy val UserVisible = NS #> "UserVisible"
-
-    /**
-     * Modification time (property)
-     * The modification time of the subject (usually a file)
-     */
-    lazy val sModificationTime = NS #> "sModificationTime"
-
-    /**
-     * Ontology (class)
-     * Metadata about the ontology
-     */
-    lazy val tology = NS ## "tology"
-
-    /**
-     * Ontology namespace abbreviation (property)
-     * Abbreviation for the ontology's namespace
-     */
-    lazy val sNamespaceAbbreviation = NS #> "sNamespaceAbbreviation"
 
 
 }
